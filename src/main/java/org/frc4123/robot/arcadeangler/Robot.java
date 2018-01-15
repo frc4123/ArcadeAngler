01f237d4a100664ab436370c7f9a63f6e1bcfce1
Unknown <frc4123devs@gmail.com>
2018-01-15 15:10:21 -0800

    Started implementing BMU control
    needs to have speed controllers added

--- src/main/java/org/frc4123/robot/arcadeangler/Robot.java
+++ src/main/java/org/frc4123/robot/arcadeangler/Robot.java
@@ -2,8 +2,14 @@ package org.frc4123.robot.arcadeangler;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import org.frc4123.robot.arcadeangler.control.Joysticks;
+import org.frc4123.robot.arcadeangler.Constants;
+import edu.wpi.first.wpilibj.Spark;
 
 public class Robot extends IterativeRobot {
+
+    //Controllers of doom
+    Joysticks mJoysticks = new Joysticks();
+
     @Override
     public void robotInit() { }
 
@@ -28,8 +34,12 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
-
-
+        if (mJoysticks.getGrabberStatus()== Joysticks.Grabber.INTAKE) {
+            //TODO: Set spark mc to intake
+        }else if (mJoysticks.getGrabberStatus() == Joysticks.Grabber.EJECT){
+            //TODO: Set spark mc to eject
+        }else {
+            //TODO: Set spark mc to stop
         }
     }
 
