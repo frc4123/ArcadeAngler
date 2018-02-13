16fb93cae033d1d61df1be5ed6eafd44a344e75f
Unknown <frc4123devs@gmail.com>
2018-02-12 19:07:07 -0800

    Began JoystickConstants class to simplify chaning controls in future
    Added all xbox controller buttons.

---
+++ src/main/java/org/frc4123/robot/arcadeangler/control/JoystickConstants.java
@@ -0,0 +1,23 @@
+package org.frc4123.robot.arcadeangler.control;
+
+public class JoystickConstants {
+
+    //**XBOX Controller**//
+    //Button Constants
+    public static final int kXBOX_A = 0;
+    public static final int kXBOX_B = 1;
+    public static final int kXBOX_X = 2;
+    public static final int kXBOX_Y = 3;
+    public static final int kXBOX_LBump = 4;
+    public static final int kXBOX_RBump = 5;
+    public static final int kXBOX_Back = 6;
+    public static final int kXBOX_Start = 7;
+    public static final int kXBOX_LJoyPress = 8;
+    public static final int kXBOX_RJoyPress = 9;
+    //Axis Constants
+
+    //**Logitech Controller**//
+    //Button Constants
+    //Axis Constants
+
+}
