e4953d5bf15de25d9918f159d9647bb979275fba
Unknown <frc4123devs@gmail.com>
2018-02-13 16:47:09 -0800

    Completed adding all of the f310 and x3D controller constants

--- src/main/java/org/frc4123/robot/arcadeangler/control/JoystickConstants.java
+++ src/main/java/org/frc4123/robot/arcadeangler/control/JoystickConstants.java
@@ -2,6 +2,15 @@ package org.frc4123.robot.arcadeangler.control;
 
 public class JoystickConstants {
 
+    /*Currently sourced from
+    * http://www.team358.org/files/programming/ControlSystem2015-2019/images/XBoxControlMapping.jpg
+    * and
+    * http://team358.org/files/programming/ControlSystem2009-/components.php
+    * and
+    * http://eidetec.com/frc-controller
+    * TODO: Double Check these values before merging with master
+    * */
+
     //**XBOX Controller**//
     //Button Constants
     public static final int kXBOX_A = 0;
@@ -24,7 +33,41 @@ public class JoystickConstants {
 
     //**Logitech F310 Game Controller**//
     //Button Constants
-
+    public static final int kF310_A = 1;
+    public static final int kF310_B = 2;
+    public static final int kF310_X = 3;
+    public static final int kF310_Y = 4;
+    public static final int kF310_LBump = 5;
+    public static final int kF310_RBump = 6;
+    public static final int kF310_Back = 7;
+    public static final int kF310_Start = 8;
+    public static final int kF310_LJoyPress = 9;
+    public static final int kF310_RJoyPress = 10;
     //Axis Constants
+    public static final int kF310_LJoyX = 1; //Left is negative
+    public static final int kF310_LJoyY = 2; //Up is negative
+    public static final int kF310_LTrigger = 3; //Only 0 to 1
+    public static final int kF310_RTrigger = 4; //Only 0 to 1
+    public static final int kF310_RJoyX = 5; //Left is negative
+    public static final int kF310_RJoyY = 6; //Up is negative
 
+    //**Logitech Extreme 3D (Flight Sim Joystick)**//
+    //Button Constants - Note: Buttons with mere numbers are numbered on the controller
+    public static final int kX3D_Trigger = 1;
+    public static final int kX3D_TriggerThumb = 2;
+    public static final int kX3D_3 = 3;
+    public static final int kX3D_4 = 4;
+    public static final int kX3D_5 = 5;
+    public static final int kX3D_6 = 6;
+    public static final int kX3D_7 = 7;
+    public static final int kX3D_8 = 8;
+    public static final int kX3D_9 = 9;
+    public static final int kX3D_10 = 10;
+    public static final int kX3D_11 = 11;
+    public static final int kX3D_12 = 12;
+    //Axis Constants
+    public static final int kX3D_X = 1; //Forward is negative
+    public static final int kX3D_Y = 1; //Left is negative
+    public static final int kX3D_Twist = 1; //Left is negative
+    public static final int kX3D_Potentiometer = 1; //Note that indicated positive on the controller is negative is code. Up is -1
 }
