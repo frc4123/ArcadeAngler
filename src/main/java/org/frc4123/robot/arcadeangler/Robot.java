ab895d4009504bbe1cb7200ad4f92994b4746e68
Unknown <frc4123devs@gmail.com>
2018-02-17 12:54:42 -0800

    Implemented basic testing drive

--- src/main/java/org/frc4123/robot/arcadeangler/Robot.java
+++ src/main/java/org/frc4123/robot/arcadeangler/Robot.java
@@ -1,6 +1,10 @@
 package org.frc4123.robot.arcadeangler;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import org.frc4123.robot.arcadeangler.control.Joysticks;
 import org.frc4123.robot.arcadeangler.Constants;
 import edu.wpi.first.wpilibj.Spark;
@@ -16,14 +20,31 @@ public class Robot extends IterativeRobot {
     PowerCubeManipulator mPWRCubeMan = new PowerCubeManipulator();
     Elevator elevator = new Elevator();
 
+    //Drive
+    WPI_TalonSRX l_master = new WPI_TalonSRX(Constants.id_driveLeftMaster);
+    WPI_TalonSRX l_slave = new WPI_TalonSRX(Constants.id_driveLeftSlave);
+    WPI_TalonSRX r_master = new WPI_TalonSRX(Constants.id_driveRightMaster);
+    WPI_TalonSRX r_slave = new WPI_TalonSRX(Constants.id_driveRightSlave);
+
+    SpeedControllerGroup left = new SpeedControllerGroup(l_master, l_slave);
+    SpeedControllerGroup right = new SpeedControllerGroup(r_master, r_slave);
+
+    DifferentialDrive mDrive = new DifferentialDrive(left, right);
+
     @Override
     public void robotInit() {
+        l_master.set(ControlMode.PercentOutput, 0);
+        l_slave.follow(l_master);
+        r_master.set(ControlMode.PercentOutput, 0);
+        r_slave.follow(r_master);
         System.out.println("Robot.robotInit");
     }
 
     @Override
     public void disabledInit() {
+        mDrive.stopMotor();
         System.out.println("Robot.disabledInit");
+    }
 
 
     }
@@ -37,7 +58,6 @@ public class Robot extends IterativeRobot {
     @Override
     public void testInit() { }
 
-
     @Override
     public void disabledPeriodic() {
         elevator.stop();
@@ -48,6 +68,8 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
+
+        mDrive.arcadeDrive(mJoysticks.getThrottle(), mJoysticks.getTurn());
 
         //PowerCube Manipulator Commands
         mPWRCubeMan.setIntakeSpeed(mJoysticks.getIntakeSpeed());
