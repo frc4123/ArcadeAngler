7189a925bd2c269c6c8f32310221f9b0d1f18583
Trevor Peitzman <trevor.peitzman@gmail.com>
2018-02-19 23:27:44 -0800

    Added Elevate Action

---
+++ src/main/java/org/frc4123/robot/arcadeangler/auto/actions/ElevateAction.java
@@ -0,0 +1,50 @@
+package org.frc4123.robot.arcadeangler.auto.actions;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.frc4123.robot.arcadeangler.subsystems.Elevator;
+
+public class ElevateAction implements Action {
+
+    private final Elevator mElevator = new Elevator();
+
+    private enum Position {SWITCH, SCALE, DOWN};
+    private Elevator.Mode elevateMode;
+    private boolean isElevationFinished = false;
+
+    public ElevateAction(Position position) {
+
+        switch (position) {
+            case SWITCH:
+                elevateMode = Elevator.Mode.HIGH;
+                break;
+            case SCALE:
+                elevateMode = Elevator.Mode.MEDIUM;
+                break;
+            case DOWN:
+                elevateMode = Elevator.Mode.LOW;
+                break;
+        }
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        isElevationFinished = true;
+        return mElevator.hasReachedSetpoint();
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void done() {
+        //TODO: Consider necessity of this
+        SmartDashboard.putBoolean("isElevateAction Complete?", isElevationFinished);
+    }
+
+    @Override
+    public void start() {
+        mElevator.setMode(elevateMode);
+    }
+}
