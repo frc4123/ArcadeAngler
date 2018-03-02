0dff7fc8fad51601409f28d852729e71107285b2
Trevor Peitzman <trevor.peitzman@gmail.com>
2018-03-02 06:57:59 -0800

    Reformatting & Added isDone getters to G2

--- src/main/java/org/frc4123/robot/arcadeangler/subsystems/PneumaticGrabber.java
+++ src/main/java/org/frc4123/robot/arcadeangler/subsystems/PneumaticGrabber.java
@@ -9,27 +9,48 @@ public class PneumaticGrabber {
     DoubleSolenoid armOpener = new DoubleSolenoid(2, 3);
     DoubleSolenoid grabFlipper = new DoubleSolenoid(0, 1);
 
-    public void foldArmsDown(){
+    public void foldArmsDown() {
         grabFlipper.set(DoubleSolenoid.Value.kReverse);
     }
 
-    public void foldArmsUp(){
+    public void foldArmsUp() {
         grabFlipper.set(DoubleSolenoid.Value.kForward);
     }
 
-    public void open(){
+    public void open() {
         armOpener.set(DoubleSolenoid.Value.kForward);
     }
 
-    public void close(){
+    public void close() {
         armOpener.set(DoubleSolenoid.Value.kReverse);
     }
 
-    public void stopFolding(){
+    public void stopFolding() {
         grabFlipper.set(DoubleSolenoid.Value.kOff);
     }
 
-    public void stopGrabbingCube(){
+    public void stopGrabbingCube() {
         armOpener.set(DoubleSolenoid.Value.kOff);
     }
+
+    public boolean isArmsClosed() {
+        if (armOpener.get() == DoubleSolenoid.Value.kReverse) {
+            return true;
+        } else if (armOpener.get() == DoubleSolenoid.Value.kForward) {
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isArmsDown() {
+        if (grabFlipper.get() == DoubleSolenoid.Value.kReverse) {
+            return true;
+        } else if (grabFlipper.get() == DoubleSolenoid.Value.kForward) {
+            return false;
+        } else {
+            return false;
+        }
+    }
+
 }
