abfac47e33691c779119469ed3f5a326f19fc329
Trevor Peitzman <trevor.peitzman@gmail.com>
2018-03-11 15:20:59 -0700

    Created Extremely basic CrossBaselineMode
    Still needs the actual mp csv 
    potentially consider leaving these in the github folder to be committed?

---
+++ src/main/java/org/frc4123/robot/arcadeangler/auto/modes/CrossBaselineMode.java
@@ -0,0 +1,18 @@
+package org.frc4123.robot.arcadeangler.auto.modes;
+
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import org.frc4123.robot.arcadeangler.auto.actions.DriveMPAction;
+
+import java.io.File;
+
+public class CrossBaselineMode extends AutoModeBase {
+
+    @Override
+    protected void routine() { //TODO: Ensure files are named correctly and properly generated
+        Trajectory left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/crossbaseline_left_detailed.csv"));
+        Trajectory right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/crossbaseline_right_detailed.csv"));
+
+        runAction(new DriveMPAction(left, right));
+    }
+}
