f6a3fca2d7b0f19236a223585faf2b29f72076e3
Trevor Peitzman <trevor.peitzman@gmail.com>
2018-03-12 07:04:26 -0700

    Started on SwitchCube Mode

---
+++ src/main/java/org/frc4123/robot/arcadeangler/auto/modes/SwitchCubeDeposit.java
@@ -0,0 +1,35 @@
+package org.frc4123.robot.arcadeangler.auto.modes;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import org.frc4123.robot.arcadeangler.auto.actions.DriveMPAction;
+
+import java.io.File;
+
+public class SwitchCubeDeposit extends AutoModeBase {
+    @Override
+    protected void routine() {
+        String gameData;
+        gameData = DriverStation.getInstance().getGameSpecificMessage();
+
+        Trajectory left;
+        Trajectory right;
+        
+        if(gameData.length() > 0)
+        {
+            if(gameData.charAt(0) == 'L')
+            {
+                //Put left auto code here
+                left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_detailed.csv"));
+                right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_detailed.csv"));
+            } else {
+                //Put right auto code here
+                left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_detailed.csv"));
+                right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_detailed.csv"));
+            }
+        }
+
+        runAction(new DriveMPAction(left, right));
+    }
+}
