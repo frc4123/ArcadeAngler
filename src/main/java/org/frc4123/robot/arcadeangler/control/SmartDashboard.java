e05e8b473970d9e997344149e13b6e73b5f2a6c0
Trevor Peitzman <trevor.peitzman@gmail.com>
2018-03-13 06:52:10 -0700

    Created Separeate smartdash class. Placed rough outline of what I want
    @adriancampos should I be extending the real SmartDashboard here?

---
+++ src/main/java/org/frc4123/robot/arcadeangler/control/SmartDashboard.java
@@ -0,0 +1,14 @@
+package org.frc4123.robot.arcadeangler.control;
+
+public class SmartDashboard extends edu.wpi.first.wpilibj.smartdashboard.SmartDashboard {
+
+    public void SmartDashboard(){
+        //TODO
+    }
+    public void getAutoInfo(){
+        //TODO: place auto info from sd
+    }
+    public void setAutoInfo(){
+        //TODO: place auto info into sd
+    }
+}
