a2c58a09fe612d8edea019001b1209ed91528a89
Unknown <frc4123devs@gmail.com>
2018-03-15 11:12:18 -0700

    Added a print if no mp traj is loaded to avoid crashing

--- src/main/java/org/frc4123/robot/arcadeangler/auto/modes/SwitchCubeDepositMode.java
+++ src/main/java/org/frc4123/robot/arcadeangler/auto/modes/SwitchCubeDepositMode.java
@@ -24,74 +24,75 @@ public class SwitchCubeDepositMode extends AutoModeBase {
         String gameData;
         gameData = DriverStation.getInstance().getGameSpecificMessage();
 
-        String robotPosition = mSmartDashboard.getSelectedRobotStartingPosition();
-
         //Possible Values: R - right, L - left, C - center.
-        //TODO: Test this to ensure it replaces like I thought it would
-
+        String robotPosition = mSmartDashboard.getSelectedRobotStartingPosition();
+        
         //Why doesn't this work?
 //        Trajectory reverse_left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/back_up_2ft_left_detailed.csv"));
 //        Trajectory reverse_right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/back_up_2ft_right_detailed.csv"));
 
-        //TODO: Ensure there are actually paths for all these OR ELSE EVERYTHING WILL CRASH
-        if (gameData.length() > 0) {
-            if (gameData.charAt(0) == 'L') {
-                if (robotPosition == "L") {
-                    //Put left auto code here
-                    left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_leftsw_left_detailed.csv"));
-                    right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_leftsw_right_detailed.csv"));
-                } else if (robotPosition == "R") {
-                    //Put left auto code here
-                    left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_leftsw_left_detailed.csv"));
-                    right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_leftsw_right_detailed.csv"));
-                } else if (robotPosition == "C") {
-                    //Put left auto code here
-                    left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/center_leftsw_left_detailed.csv"));
-                    right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/center_leftsw_right_detailed.csv"));
-                }
-            }
-            if (gameData.charAt(0) == 'R') {
-                if (robotPosition == "L") {
-                    //Put left auto code here
-                    left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_rightsw_left_detailed.csv"));
-                    right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_rightsw_right_detailed.csv"));
-                } else if (robotPosition == "R") {
-                    //Put left auto code here
-                    left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_rightsw_left_detailed.csv"));
-                    right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_rightsw_right_detailed.csv"));
-                } else if (robotPosition == "C") {
-                    //Put left auto code here
-                    left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/center_rightsw_left_detailed.csv"));
-                    right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/center_rightsw_right_detailed.csv"));
-                }
-            }
+        if (left == null || right == null) {
+            System.out.println("No Motion Profile loaded. Doing nothing to avoid crash.");
         } else {
-            //TESTPLZ
-            System.out.println("Field piece position not recieved. Driving to baseline.");
-            left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/crossbaseline_left_detailed.csv"));
-            right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/crossbaseline_right_detailed.csv"));
+            if (gameData.length() > 0) {
+                if (gameData.charAt(0) == 'L') {
+                    if (robotPosition == "L") {
+                        //Put left auto code here
+                        left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_leftsw_left_detailed.csv"));
+                        right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_leftsw_right_detailed.csv"));
+                    } else if (robotPosition == "R") {
+                        //Put left auto code here
+                        left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_leftsw_left_detailed.csv"));
+                        right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_leftsw_right_detailed.csv"));
+                    } else if (robotPosition == "C") {
+                        //Put left auto code here
+                        left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/center_leftsw_left_detailed.csv"));
+                        right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/center_leftsw_right_detailed.csv"));
+                    }
+                }
+                if (gameData.charAt(0) == 'R') {
+                    if (robotPosition == "L") {
+                        //Put left auto code here
+                        left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_rightsw_left_detailed.csv"));
+                        right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/left_rightsw_right_detailed.csv"));
+                    } else if (robotPosition == "R") {
+                        //Put left auto code here
+                        left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_rightsw_left_detailed.csv"));
+                        right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/right_rightsw_right_detailed.csv"));
+                    } else if (robotPosition == "C") {
+                        //Put left auto code here
+                        left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/center_rightsw_left_detailed.csv"));
+                        right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/center_rightsw_right_detailed.csv"));
+                    }
+                }
+            } else {
+                //TESTPLZ
+                System.out.println("Field piece position not recieved. Driving to baseline.");
+                left = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/crossbaseline_left_detailed.csv"));
+                right = Pathfinder.readFromCSV(new File("/home/lvuser/trajectories/crossbaseline_right_detailed.csv"));
+                runAction(new DriveMPAction(left, right));
+                stop();
+            }
+
+            System.out.println("Flipping grabber up.");
+            runAction(new FlipperPneumaticAction(FlipperPneumaticAction.State.UP));
+            System.out.println("Driving to target.");
             runAction(new DriveMPAction(left, right));
-            stop();
-        }
+            System.out.println("Elevating to switch.");
+            runAction(new ElevateAction(ElevateAction.Position.SWITCH));
 
-        System.out.println("Flipping grabber up.");
-        runAction(new FlipperPneumaticAction(FlipperPneumaticAction.State.UP));
-        System.out.println("Driving to target.");
-        runAction(new DriveMPAction(left, right));
-        System.out.println("Elevating to switch.");
-        runAction(new ElevateAction(ElevateAction.Position.SWITCH));
+            //TODO: determine necessity of the following line
+            //runAction(new FlipperPneumaticAction(FlipperPneumaticAction.State.DOWN));
 
-        //TODO: determine necessity of the following line
-        //runAction(new FlipperPneumaticAction(FlipperPneumaticAction.State.DOWN));
+            System.out.println("Opening arms.");
+            runAction(new ArmsPneumaticAction(ArmsPneumaticAction.State.OPEN));
 
-        System.out.println("Opening arms.");
-        runAction(new ArmsPneumaticAction(ArmsPneumaticAction.State.OPEN));
-
-        //The below is just icing on the cake. Remove if breaking things
-        //TODO: Determine if reverse motion profile even works - doesn't work rn
+            //The below is just icing on the cake. Remove if breaking things
+            //TODO: Determine if reverse motion profile even works - doesn't work rn
 //        runAction(new ElevateAction(ElevateAction.Position.DOWN));
 //        runAction(new DriveMPAction(reverse_left, reverse_right));
 //        runAction(new FlipperPneumaticAction(FlipperPneumaticAction.State.DOWN));
 //        done();
+        }
     }
 }
